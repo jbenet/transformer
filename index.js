1427db6908673b29a0f05271a8151c9cc850bc64
Juan Batiz-Benet <juan@benet.ai>
2014-04-23 14:26:00 -0700

    first transformer works!
    ```
    node
    > var transformer = require('./index');
    > var json2xml = transformer('json-codec', 'xml-codec');
    > var input = JSON.stringify({foo:'bar'});
    > json2xml(input)
    '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\n<foo>bar</foo>'
    ```

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var Type = require(path.join(__dirname, 'js', 'type'));
 var Codec = require(path.join(__dirname, 'js', 'codec'));
 var Conversion = require(path.join(__dirname, 'js', 'conversion'));
 var Loader = require(path.join(__dirname, 'js', 'loader'));
+var _ = require('underscore');
 
 module.exports = transformer
 
@@ -11,31 +12,74 @@ module.exports = transformer
 //
 // var b_data = transformer.convert(schema_a, schema_b, a_data)
 
-function transformer(type_from, type_to, data_from) {
+function transformer(from, to, data_from) {
   switch (arguments.length) {
   case 3:
-    return transformer.convert(type_from, type_to, data_from);
+    return transformer.transform(from, to, data_from);
   case 2:
-    return transformer.converter(type_from, type_to);
+    return transformer.transformer(from, to);
   default:
     throw new Error('transformer error: incorrect arguments to transformer.')
   }
 }
 
 
-transformer.converter = function(schema_from, schema_to) {
-  schema_from = Type(schema_from);
-  schema_to = Type(schema_to);
+transformer.transformer = function(from, to) {
+  from = transformer.coerce_object(from);
+  to = transformer.coerce_object(to);
 
-  return function() {
-    // function to convert from -> to
+  if (!(from instanceof Type || from instanceof Codec)) {
+    throw new Error('conversion source must be Type or Codec.');
+  }
+
+  if (!(to instanceof Type || to instanceof Codec)) {
+    throw new Error('conversion target must be Type or Codec.');
+  }
+
+  // default to no conversion. (from or to is codec).
+  var convert = function(input) { return input; }
+
+  // if from && to are Types, find their conversion function.
+  if (from instanceof Type && to instanceof Type) {
+    // not implemented yet.
+  }
+
+  return function(input_data) {
+    var d = from.decode(input_data);
+    var c = convert(d);
+    var e = to.encode(c);
+    return e;
   }
 }
 
+transformer.transform = function(from, to, data_from) {
+  // create transformer + apply it.
+  return transformer.transformer(from, to)(data_from);
+}
 
-transformer.convert = function(schema_from, schema_to, data_from) {
-  var converter = transformer.converter(schema_from, schema_to);
-  return converter(data_from);
+transformer.coerce_object = function(obj) {
+  // string? load module.
+  if (_.isString(obj))
+    return Loader(obj);
+
+  // transformer object? all good.
+  if (obj instanceof Type ||
+      obj instanceof Codec ||
+      obj instanceof Conversion) {
+    return obj;
+  }
+
+  // raw object with @type? construct.
+  if (_.isObject(obj) && obj['@type']) {
+    switch (obj['@type']) {
+    case 'Type': return Type(obj);
+    case 'Codec': return Codec(obj);
+    case 'Conversion': return Conversion(obj);
+    }
+  }
+
+  // wat?
+  throw new Error('transformer: unknown input object.');
 }
 
 transformer.Type = Type;
