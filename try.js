9ebd9765d6d126e492f3bde6a8819bf13bd4c746
Juan Batiz-Benet <juan@benet.ai>
2014-04-25 00:02:51 -0700

    nicer try.js
    ```
    % node try.js
    --> jsonml-xml-codec 2 json-codec
    input:      <foo>bar</foo>
    a2b(input):   ["foo","bar"]
    b2a(a2b(input)):  <foo>bar</foo>
    SUCCESS ✓
    
    --> unix-time-date-codec 2 iso-date-codec
    input:      1398408254
    a2b(input):   2014-04-25T06:44:14.000Z
    b2a(a2b(input)):  1398408254
    SUCCESS ✓
    
    ```

--- try.js
+++ try.js
@@ -1,7 +1,20 @@
-var t = require('./index');
-var json2xml = t('json-codec', 'jsonml-xml-codec');
-var xml2json = t('jsonml-xml-codec', 'json-codec');
-var xmlstr = '<foo>bar</foo>'
-console.log(xmlstr + ' // xmlstr');
-console.log(xml2json(xmlstr) + '  // xml2json(xmlstr)');
-console.log(json2xml(xml2json(xmlstr)) + ' // json2xml(xml2json(xmlstr))');
+var transformer = require('./index');
+var colors = require('colors');
+
+var convert = function(a, b, data) {
+  var a2b = transformer(a, b);
+  var b2a = transformer(b, a);
+  console.log('--> '+ a +' 2 '+ b);
+  console.log('input:\t\t\t' + data);
+  console.log('a2b(input):\t\t' + a2b(data));
+  console.log('b2a(a2b(input)):\t' + b2a(a2b(data)));
+  if (b2a(a2b(data)) === data) {
+    console.log('✓ SUCCESS'.green);
+  } else {
+    console.log('✖ FAILURE'.red);
+  }
+  console.log('');
+};
+
+convert('jsonml-xml-codec', 'json-codec', '<foo>bar</foo>');
+convert('unix-time-date-codec', 'iso-date-codec', 1398408254);
