37315f25bf54b42072f9ae1b850eed7e5b39e805
Juan Batiz-Benet <juan@benet.ai>
2014-04-29 16:26:14 -0700

    ip-address codecs

--- js/transformer/ip-address-int.js
+++ js/transformer/ip-address-int.js
@@ -4,20 +4,18 @@ var Codec = require('../codec');
 
 var IPCodec = module.exports = new Codec({
   // @context and @type filled in automatically.
-  '@id': 'transformer/ip-address-buffer',
-  'description': 'IP Address string encoding: 127.0.0.1'
+  '@id': 'transformer/ip-address-int',
+  'description': 'IP Address integer encoding: 127.0.0.1 is 2130706433'
 });
 
 IPCodec.encode = function(ipStr) {
   if (!_.isString(ipStr))
     throw new Error('TypeError: ipStr must be a string.')
 
-  return ip.toBuffer(ipStr);
+  return ip.toLong(ipStr);
 }
 
-IPCodec.decode = function(ipBuffer) {
-  if (!(ipBuffer instanceof Buffer))
-    throw new Error('TypeError: ipBuffer must be a Buffer.');
-
-  return ip.toString(ipBuffer);
+IPCodec.decode = function(int) {
+  int = parseInt(int, 10)
+  return ip.fromLong(int);
 }
